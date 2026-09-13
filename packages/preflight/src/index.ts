ee79c3ef326028248dab8081f6500faa78aec5fa
Clayton nunes <114633702+KayzenRoot@users.noreply.github.com>
2026-09-12 21:45:57 -0300

    m04: export preflight package

---
+++ packages/preflight/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./types.js";
+export * from "./canonical.js";
+export * from "./environment.js";
+export * from "./git.js";
+export * from "./provider.js";
+export * from "./toolchain.js";
+export * from "./project-state.js";
