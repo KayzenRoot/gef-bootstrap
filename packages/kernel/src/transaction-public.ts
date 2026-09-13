bfc588ae4ed4030dd8ed088eb5ef4572bffe2b84
Clayton nunes <114633702+KayzenRoot@users.noreply.github.com>
2026-09-13 00:17:56 -0300

    M05: route rollback through current-authorization facade

--- packages/kernel/src/transaction-public.ts
+++ packages/kernel/src/transaction-public.ts
@@ -11,8 +11,8 @@ export {
   validateTransactionPlanRuntime,
   verifyTransactionPlanDigest,
 } from "./transaction-plan-engine.js";
-export { rollbackIntentFor, rollbackTransaction } from "./transaction-rollback-certified.js";
-export type { RollbackTransactionInput } from "./transaction-rollback-certified.js";
+export { rollbackIntentFor, rollbackTransaction } from "./transaction-rollback-authorized.js";
+export type { RollbackTransactionInput } from "./transaction-rollback-authorized.js";
 export { evaluateStateBinding } from "./transaction-state.js";
 export type * from "./transaction-ports.js";
 export type * from "./transaction-recovery-types.js";
