55ff88ae52b640d7d2b3c647584f3509997d8eb0
Clayton nunes <114633702+KayzenRoot@users.noreply.github.com>
2026-09-12 21:01:55 -0300

    fix(m03): export repository projection validator

--- packages/project-identity/src/index.ts
+++ packages/project-identity/src/index.ts
@@ -3,6 +3,7 @@ export * from "./canonical.js";
 export * from "./project-id.js";
 export * from "./project-config.js";
 export * from "./repository.js";
+export * from "./projection.js";
 export * from "./fingerprint.js";
 export * from "./collision.js";
 export * from "./transitions.js";
