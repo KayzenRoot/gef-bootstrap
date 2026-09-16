658311e6fbd3e58bae418ebdf08ef9cf26a94a1a
Clayton nunes <114633702+KayzenRoot@users.noreply.github.com>
2026-09-16 06:41:34 -0300

    M26: expose public HEDS API

---
+++ packages/heds-delta-review/src/public.ts
@@ -0,0 +1,7 @@
+export*from'./types.js';
+export{M26_MECHANISMS}from'./registry.js';
+export*from'./s01-delta.js';
+export*from'./s02-source.js';
+export*from'./s03-review.js';
+export*from'./s04-gates.js';
+export*from'./s05-verdict.js';
